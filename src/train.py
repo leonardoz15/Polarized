1fef9f2335df78b84911b98ee20a856d1dde76cb
unknown <leonardoz@allegheny.edu>
2020-03-04 13:27:45 -0500

    Final training method

--- src/train.py
+++ src/train.py
@@ -20,16 +20,25 @@ keyword_tweets = pd.read_csv("../data/keyword-tweets.txt", sep="\t", header=None
 # Set column headers
 keyword_tweets.columns = ['subject', 'tweet']
 
+# Import combined-tweets.txt as tab-delimited csv
+combined_tweets = pd.read_csv("../data/combined-tweets.txt", sep="\t", header=None)
+# Set column headers
+combined_tweets.columns = ['subject', 'tweet']
+
 # Import keyword-tweets.txt as tab-delimited csv
 # extracted_tweets = pd.read_csv("../data/ExtractedTweets.csv")
 
+#
+# fig = plt.figure(figsize=(8,6))
+# general_tweets.groupby('subject').tweet.count().plot.bar(ylim=0)
+# plt.show()
+#
+# fig = plt.figure(figsize=(8,6))
+# keyword_tweets.groupby('subject').tweet.count().plot.bar(ylim=0)
+# plt.show()
 
 fig = plt.figure(figsize=(8,6))
-general_tweets.groupby('subject').tweet.count().plot.bar(ylim=0)
-plt.show()
-
-fig = plt.figure(figsize=(8,6))
-keyword_tweets.groupby('subject').tweet.count().plot.bar(ylim=0)
+combined_tweets.groupby('subject').tweet.count().plot.bar(ylim=0)
 plt.show()
 
 # fig = plt.figure(figsize=(8,6))
@@ -44,9 +53,9 @@ plt.show()
 # keyword_tweets = keyword_tweets.replace(to_replace ="POLIT", value = 1)
 
 # Features = tweet
-x = keyword_tweets["tweet"]
+x = combined_tweets["tweet"]
 # Target = subject
-y = keyword_tweets["subject"]
+y = combined_tweets["subject"]
 
 tfidf = TfidfVectorizer(sublinear_tf=True, min_df=3, norm='l2', encoding='latin-1', ngram_range=(1, 2), stop_words='english')
 
@@ -56,38 +65,40 @@ print(features1.shape)
 features2 = tfidf.fit_transform(keyword_tweets.tweet).toarray()
 print(features2.shape)
 
+features3 = tfidf.fit_transform(combined_tweets.tweet).toarray()
+print(features3.shape)
+
 # features3 = tfidf.fit_transform(extracted_tweets.Tweet).toarray()
 # features3.shape
 # print(features3)
 
 X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.2)
-count_vect = CountVectorizer()
-X_train_counts = count_vect.fit_transform(X_train)
-tfidf_transformer = TfidfTransformer()
-X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+X_train_tfidf = tfidf.fit_transform(X_train)
+# tfidf_transformer = TfidfTransformer()
+# X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 
 
-string = "Pizza"
+string = "Don't vote for Bernie, hes a communist"
 print("String to test :\n", string)
 
 print("Testing with multinomial NB:")
 
 # MultinomialNB
 clf = MultinomialNB().fit(X_train_tfidf, y_train)
-print(clf.predict(count_vect.transform([string])))
+print(clf.predict(tfidf.transform([string])))
 
-# Linear SVM with regulization parameter C
+# Linear SVM with regulization parameter
 clf2 = svm.SVC(kernel="linear", C=2)
 clf2.fit(X_train_tfidf, y_train)
 
 print("Testing with linear SVM:")
-print(clf2.predict(count_vect.transform([string])))
+print(clf2.predict(tfidf.transform([string])))
 
-y_predict = clf.predict(count_vect.transform(X_test))
-y_predict2 = clf2.predict(count_vect.transform(X_test))
-
-print(classification_report(y_test, clf2.predict(count_vect.transform(X_test))))
-
-accNB = metrics.accuracy_score(y_test, y_predict)
-accSVM = metrics.accuracy_score(y_test, y_predict2)
-print("Accuracy NB : ", accNB, "\nAccuracy SVM : ", accSVM)
+# y_predict = clf.predict(count_vect.transform(X_test))
+# y_predict2 = clf2.predict(count_vect.transform(X_test))
+#
+# print(classification_report(y_test, clf2.predict(count_vect.transform(X_test))))
+#
+# accNB = metrics.accuracy_score(y_test, y_predict)
+# accSVM = metrics.accuracy_score(y_test, y_predict2)
+# print("Accuracy NB : ", accNB, "\nAccuracy SVM : ", accSVM)
